78b9dcd61c32fdedffd3839ede17733e7593d95e
Antonio Oliva Carceles <116766832+olixva@users.noreply.github.com>
2024-02-03 14:47:52 +0100

    Curso estrategias de nombrado termiado

--- 11. Estrategias de nombrado/src/main/java/com/hibernate/model/SmartPhone.java
+++ 11. Estrategias de nombrado/src/main/java/com/hibernate/model/SmartPhone.java
@@ -1,17 +1,18 @@
 package com.hibernate.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+
 
 @Entity
+//@Table(name = "smart_phone")
 public class SmartPhone {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
+
+//    @Column(name = "phone_number")
     private String phoneNumber;
 
     public SmartPhone() {
