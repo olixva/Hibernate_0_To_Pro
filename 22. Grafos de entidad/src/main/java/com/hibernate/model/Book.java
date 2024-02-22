1253517edd2f3d5239ef31e7b7f39701b9b4a8db
Antonio Oliva Carceles <116766832+olixva@users.noreply.github.com>
2024-02-22 09:34:49 +0100

    Cursos de grafos de entidad terminado

--- 22. Grafos de entidad/src/main/java/com/hibernate/model/Book.java
+++ 22. Grafos de entidad/src/main/java/com/hibernate/model/Book.java
@@ -10,56 +10,29 @@ public class Book {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
+    private Long id;
 
     private String title;
 
-    private Double price;
-
-    @Column(name = "number_pages")
-    private Integer numberOfPages;
-
-    private boolean published;
-
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "author_id")
     private Author author;
 
-    @ManyToMany
-    @JoinTable(
-            name = "book_category",
-            joinColumns = @JoinColumn(name = "book_id", referencedColumnName = "id"),
-            inverseJoinColumns = @JoinColumn(name = "category_id", referencedColumnName = "id")
-    )
-    private Set<Category> categories = new HashSet<>();
+    @OneToMany(mappedBy = "book")
+    private Set<Chapter> chapters = new HashSet<>();
 
     public Book() {
     }
 
-    public Book(String title, Double price, Integer numberOfPages, boolean published, Author author) {
+    public Book(String title, Author author) {
         this.title = title;
-        this.price = price;
-        this.numberOfPages = numberOfPages;
-        this.published = published;
         this.author = author;
     }
 
-    @Override
-    public String toString() {
-        return "Book{" +
-                "id=" + id +
-                ", title='" + title + '\'' +
-                ", price=" + price +
-                ", numberOfPages=" + numberOfPages +
-                ", published=" + published +
-                '}';
-    }
-
-    public long getId() {
+    public Long getId() {
         return id;
     }
 
-    public void setId(long id) {
+    public void setId(Long id) {
         this.id = id;
     }
 
@@ -71,30 +44,6 @@ public class Book {
         this.title = title;
     }
 
-    public Double getPrice() {
-        return price;
-    }
-
-    public void setPrice(Double price) {
-        this.price = price;
-    }
-
-    public Integer getNumberOfPages() {
-        return numberOfPages;
-    }
-
-    public void setNumberOfPages(Integer numberOfPages) {
-        this.numberOfPages = numberOfPages;
-    }
-
-    public boolean isPublished() {
-        return published;
-    }
-
-    public void setPublished(boolean published) {
-        this.published = published;
-    }
-
     public Author getAuthor() {
         return author;
     }
@@ -103,11 +52,19 @@ public class Book {
         this.author = author;
     }
 
-    public Set<Category> getCategories() {
-        return categories;
+    public Set<Chapter> getChapters() {
+        return chapters;
     }
 
-    public void setCategories(Set<Category> categories) {
-        this.categories = categories;
+    public void setChapters(Set<Chapter> chapters) {
+        this.chapters = chapters;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                '}';
     }
 }
